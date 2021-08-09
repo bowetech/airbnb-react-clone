aab101404a6390cdf04499da27944197c143f634
Clive Stewart <clivestewart@hotmail.com>
2021-08-08 21:10:54 -0400

    day four build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	images: {
 		domains: ["links.papareact.com", "dominicaexplorer.com"],
+	},
+	env: {
+		mapbox_key: 'pk.eyJ1IjoiYm93ZXRlY2giLCJhIjoiY2tzM285bm9qMGZlYjJubXNtdGU5aTlhcyJ9._gRUDftkzC5kNdAxUiNIFw'
 	}
 };
